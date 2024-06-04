d51bb7a2b46e65349b64a27a0940cc37f40d48ed
gabrielsemnome <g_bdon@hotmail.com>
2024-06-03 23:16:22 -0300

    desenvolvendo a classe conta

--- src/com/bancodigital/contas/Conta.java
+++ src/com/bancodigital/contas/Conta.java
@@ -1,19 +1,36 @@
 package com.bancodigital.contas;
+import  cliente.Cliente
 
 public class Conta implements IConta{
     //atributos
-    private int agencia;
-    private int numero;
-    
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected Cliente cliente;
+
+    public Conta (Cliente cliente) {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
     //metodos
     @Override
     public void sacar(double valor) {
-
+        if (saldo >= valor){
+            saldo -= valor;
+        }else{
+            System.out.println("Não há saldo o suficiente para este saque!");
+        }
     }
 
     @Override
     public void depositar(double valor) {
-
+        saldo += valor;
+        System.out.printf("Foi depositado R$%.2f",valor);
     }
 
     @Override
